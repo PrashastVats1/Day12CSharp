51bf3945bfce9c3ac8e306890e7003acea719fe1
PrashastVats1 <prashastvats@gmail.com>
2023-07-25 14:58:30 +0530

    Merge, Shell and Quick Sort

---
+++ ConAppShellSort/ConAppShellSort/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppShellSort
+{
+    internal class Program
+    {
+        public static void ShellSort(int[] arr)
+        {
+            int n = arr.Length;
+            int gap = n / 2;
+            while (gap > 0)
+            {
+                for(int i = gap; i < n; i++)
+                {
+                    int temp = arr[i];
+                    int j = i;
+                    while (j >= gap && arr[j - gap] > temp)
+                    {
+                        arr[j] = arr[j - gap];
+                        j -= gap;
+                    }
+                    arr[j] = temp;
+                    Console.WriteLine($"After iteration with gap = {gap}:");
+                    Print(arr);
+                }
+                gap /= 2;
+            }
+        }
+        public static void Print(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i]+" ");
+            }
+            Console.WriteLine("\n");
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = { 7, 4, 5, 2, 45, 78, 90, 12 };
+            Console.WriteLine("Print array without sorting");
+            Print(arr);
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            ShellSort(arr);
+            stopwatch.Stop();
+            Console.WriteLine("After shell sort");
+            Print(arr);
+            Console.WriteLine($"Array Size: {arr.Length} \t Time Taken: {stopwatch.Elapsed.TotalMilliseconds} ms");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConAppShellSort/ConAppShellSort/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ConAppShellSort")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("ConAppShellSort")]
+[assembly: AssemblyCopyright("Copyright © HP 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7ee120c2-8ee6-4fd0-942f-1010e9fed821")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
